1df4629109ee0ac40dac6669bf3105ceda0d743b
Sanjay-Khadka <sanjay1khadka@gmail.com>
2023-01-25 21:18:23 +0545

    style:cart screen design

---
+++ screens/cartScreen.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel=stylesheet href="../css/cart.css" />
+    <title>Cart</title>
+</head>
+
+<body>
+    <div class="cart">
+        <h2>Cart</h2>
+        <table>
+  <tr>
+    <th>Product Image</th>
+    <th>Name</th>
+    <th>Size</th>
+    <th>Quantity</th>
+    <th>Price</th>
+    <th>Action</th>
+  </tr>
+  <tr>
+    <td><img src="https://calibershoes.sgp1.digitaloceanspaces.com/uploads/2022/06/06135153/1-6-600x450.jpg" alt="product1" style="height:40px;width:40px"></td>
+    <td>Product 1</td>
+    <td>Medium</td>
+    <td>2</td>
+    <td>$20</td>
+    <td class="action">
+      <button><img src="../images/edit3.svg"/></button>
+      <button><img src="../images/x-circle.svg"/></button>
+    </td>
+  </tr>
+  <tr>
+    <td><img src="https://calibershoes.sgp1.digitaloceanspaces.com/uploads/2022/12/19131405/IMG_4136.jpg" alt="product2" style="height:40px;width:40px"></td>
+    <td>Product 2</td>
+    <td>Large</td>
+    <td>1</td>
+    <td>$30</td>
+    <td class="action">
+      <button><img src="../images/edit3.svg"/></button>
+      <button><img src="../images/x-circle.svg"/></</button>
+    </td>
+  </tr>
+  <tr>
+    <td><img src="https://calibershoes.sgp1.digitaloceanspaces.com/uploads/2022/12/19131405/IMG_4136.jpg" alt="product2" style="height:40px;width:40px"></td>
+    <td>Product 2</td>
+    <td>Large</td>
+    <td>1</td>
+    <td>$30</td>
+    <td class="action">
+      <button><img src="../images/edit3.svg"/></button>
+      <button><img src="../images/x-circle.svg"/></</button>
+    </td>
+  </tr>
+</table>
+    </div>
+</body>
+
+</html>
