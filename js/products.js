1a9b70156948176f37a0d84097031bad89a42ab3
Sanjay-Khadka <sanjay1khadka@gmail.com>
2023-01-28 21:05:25 +0545

    fix:switched to class and implemented methods to manipulate the variable instead of functional approach

--- js/products.js
+++ js/products.js
@@ -1,52 +1,85 @@
-function incDec  (type) {
-let value = document.getElementById("quantity_value").innerHTML;
-if(type === '+'){
-if(value>=3){
-    return;
-}
-else{
-    value++;
-document.getElementById("quantity_value").innerHTML= value;
-}
-
-}
-
-else if(type === '-'){
-    if(value<=1){
-        console.log('value is less than one');
-        return;
-    }
-    else{
-value--;
-document.getElementById("quantity_value").innerHTML= value;
-
-    }
-
-}
-}
+const productImage = document.getElementById("product-img").src;
+const productName = document.getElementById("product_name").innerHTML;
+const productCategory = document.getElementById("product_category").innerHTML;
+const productQuantity = document.getElementById("quantity_value").innerHTML;
 
 
-const addtoCart=(pname, photo, size, category, price, quantity, id)=>{
-    const PRODUCT = {
-        s_id:id,
-        s_name:pname,
-        s_photo:photo,
-        s_category:category,
-        s_price:price,
-        s_size:size,
-        s_quantity:quantity  
-    }
-    console.log(PRODUCT);
-    $.post("../includes/cart.php",PRODUCT, result => {
-       if(result === 'not logged in'){
-        alert(`${result}, redirecting to login page`);
-        window.location.href = "../screens/login.php";
-       }
-       else{
-           window.location.href="../screens/cartScreen.php";
-            
+class Product {
+  constructor(photo, name, category, quantity, price, size, id) {
+    this.name = name;
+    this.category = category;
+    this.price = price;
+    this.size = size;
+    this.quantity = quantity;
+    this.photo = photo;
+    this.id = id;
+  }
+
+  setQuantity(type) {
+    this.quantity = document.getElementById("quantity_value").innerHTML;
+    switch (type) {
+      case "+":
+        if (this.quantity >= 3) {
+          return;
+        } else {
+          this.quantity++;
+          document.getElementById("quantity_value").innerHTML = this.quantity;
         }
 
-    })
+        break;
 
+      case "-":
+        if (this.quantity <= 1) {
+          console.log("value is less than one");
+          return;
+        } else {
+          this.quantity--;
+          document.getElementById("quantity_value").innerHTML = this.quantity;
+        }
+        break;
+
+      default:
+        return;
+    }
+  }
+
+  addtoCart(productID, productPrice){
+    this.price  = this.quantity*productPrice;
+    this.id =productID;
+    const PRODUCT = {
+        s_id: this.id,
+        s_name: this.name,
+        s_photo: this.photo,
+        s_category: this.category,
+        s_price: this.price,
+        s_size: this.size,
+        s_quantity: this.quantity,
+      };
+      console.log(PRODUCT);
+      $.post("../includes/cart.php", PRODUCT, (result) => {
+        if (result === "not logged in") {
+          alert(`${result}, redirecting to login page`);
+          window.location.href = "../screens/login.php";
+        } else {
+          window.location.href = "../screens/cartScreen.php";
+        }
+      });
+  }
+
+  setSize(){
+    const elements = document.getElementsByClassName("size");
+    for (const element of elements) {
+      if (element === event.target) {
+        this.size = event.target.innerHTML;
+        console.log(this.size);
+        element.style.border = "2px solid black";
+      } else {
+        console.log(this.size);
+        element.style.border = "";
+      }
+    }
+  }
 }
+
+const product = new Product(productImage, productName, productCategory, productQuantity);
+
--- screens/products.php
+++ screens/products.php
@@ -14,7 +14,7 @@ $query = mysqli_query($connect, "SELECT * FROM Shoes WHERE s_id = '$id'");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/products.css?v=<?php echo time();?>" />
-    <script src="../js/products.js?2354456"></script>
+    <script src="../js/products.js?24566543" defer></script>
     <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
     <title>Products</title>
 </head>
@@ -39,31 +39,32 @@ $query = mysqli_query($connect, "SELECT * FROM Shoes WHERE s_id = '$id'");
             
             <div class="product_container">
             <div class="image_container">
-                <img src='.$featured['s_photo'].'
+                <img src='.$featured['s_photo'].' id="product-img"
                     style="object-fit:cover; height:100%; width:100%;border-radius:5px" />
             </div>
             <div class="details_container">
-                    <p class="product_name">'.$featured['s_name'].'</p>
-                    <p class="product_category"><span class="category">Category:  </span> '.$featured['s_category'].'</p>
+                    <p class="product_name" id="product_name">'.$featured['s_name'].'</p>
+                    <p class="product_category"> Category:<span class="category" id="product_category"> '.$featured['s_category'].' </span></p>
                     <p class="product_price">Rs '.$featured['s_price'].'</p>
                     <label class="product_size" for="size_container">Size</label>
                     <div class="size_container">
-                    <a class="size">6</a>
-                    <a class="size">7</a>
-                    <a class="size">8</a>
-                    <a class="size">9</a>
+                    <a class="size" onclick="product.setSize(event)">6</a>
+                    <a class="size" onclick="product.setSize(event)">7</a>
+                    <a class="size" onclick="product.setSize(event)">8</a>
+                    <a class="size" onclick="product.setSize(event)">9</a>
                     </div>
                     <div class="quantity_container">
                         <p class="product_quantity">Quantity</p>
                         <div class="quantity">
-                            <button class="quantity_btn" onclick= incDec("+")>+</button>
+                            <button class="quantity_btn" onclick= product.setQuantity("+")>+</button>
                             <p class="quantity_value" id="quantity_value">1</p>
-                            <button class="quantity_btn" onclick= incDec("-")>-</button>
+                            <button class="quantity_btn" onclick= product.setQuantity("-")>-</button>
                         </div>
                     </div>
                 <div class="btn_container">
                     <button class="btn" >Buy Now</button>
-                    <button class="btn" onclick ="addtoCart(\''. $featured['s_name'] .'\',\''. $featured['s_photo'] .'\', \''. $featured['s_size'] .'\', \''. $featured['s_category'] .'\', \''. $featured['s_price'] .'\', \''. $featured['s_quantity'] .'\', \''. $featured['s_id'] .'\')" >Add To Cart</button>
+                   
+                    <button class ="btn" onclick="product.addtoCart('.$featured['s_id'].','.$featured['s_price'].' )">add</button>
                 </div>
             </div>
         </div>
