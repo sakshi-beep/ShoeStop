872484825b584e5db182efd3e3a62a9de6c4e5f4
Sanjay-Khadka <sanjay1khadka@gmail.com>
2023-03-06 22:33:20 +0545

    feat:scroll to top on all.php

--- screens/all.php
+++ screens/all.php
@@ -7,11 +7,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Products</title>
     <link rel="stylesheet" href="../css/all.css?v=<?php echo time() ?>" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="../css/nav/">
+    <style>
+    #btn-scroll {
+        position: fixed;
+        padding: 10px;
+        border-radius: 50%;
+        background-color: white;
+        bottom: 60px;
+        right: 80px;
+        display: none;
+        animation: flash 2s infinite forwards;
+
+    }
+
+    @keyframes flash {
+        0% {
+            background-color: white;
+            border: 3px solid white;
+        }
+
+        100% {
+            background-color: white;
+            border: 3px solid black;
+        }
+    }
+
+    .arrow {
+        width: 40px;
+        height: 40px
+    }
+
+    @media(max-width:768px) {
+        #btn-scroll {
+            bottom: 10px;
+            padding: 10px;
+            right: 45%;
+            bottom:
+        }
+
+        .arrow {
+            width: 20px;
+            height: 20px
+        }
+    }
+    </style>
 </head>
 
 
-<body class="main">
+<body class="main" id="carousel">
     <?php include "../includes/nav.php" ?>
     <div class="container">
         <div class="desc">
@@ -41,6 +86,22 @@
             ?>
         </div>
     </div>
+
+    <a id="btn-scroll" href="#carousel"><img src="../images/arrow.svg" alt=""></a>
+    <script defer>
+    var containerAll = document.getElementById("carousel");
+
+    containerAll.onscroll = function() {
+        const scrollHeight = containerAll.scrollHeight / 3;
+        const offset = window.pageYOffset;
+        if (offset >= scrollHeight) {
+            document.getElementById("btn-scroll").style.display = "flex"
+        } else {
+            document.getElementById("btn-scroll").style.display = "none"
+        }
+    }
+    const btn = document.getElementsByClassName("btn-scroll");
+    </script>
 </body>
 
 </html>
