bacc403524cb7cdcb42cf9b6e801526608ce70a2
Sanjay-Khadka <sanjay1khadka@gmail.com>
2023-03-07 08:48:16 +0545

    fix:gap

--- screens/sportsScreen.php
+++ screens/sportsScreen.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <title>Sports</title>
     <link rel="stylesheet" href="../css/sport.css?v=<?php echo time() ?>" />
     <link rel="stylesheet" href="../css/nav.css" />
